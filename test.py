54e720a4dadb181e73a2a258100a63d06b173c9d
qlstn <qlstn9150@gmail.com>
2021-11-11 17:34:27 +0900

    1111

--- test.py
+++ test.py
@@ -17,17 +17,18 @@ import json
 (trainX, _), (testX, _) = cifar10.load_data()
 x_train, x_test = normalize_pixels(trainX, testX)
 
-
-####################################################################################################
+'''
 # test
-def comp_eval(model_str, x_test, compression_ratios, snr, mode='multiple'):
+def comp_eval(model_str, x_test, compression_ratios, snr):
     model_dic = {'SNR': [], 'Pred_Images': [], 'PSNR': [], 'SSIM': []}
     model_dic['SNR'].append(snr)
+
     for comp_ratio in compression_ratios:
         tf.keras.backend.clear_session()
         path = './checkpoints/' + model_str + '_CompRatio{0}_SNR{1}.h5'.format(comp_ratio, snr)
         autoencoder = load_model(path, custom_objects={'NormalizationNoise': NormalizationNoise})
         K.set_value(autoencoder.get_layer('normalization_noise_1').snr_db, snr)
+
         pred_images = autoencoder.predict(x_test) * 255
         pred_images = pred_images.astype('uint8')
         ssim = structural_similarity(testX, pred_images, multichannel=True)
@@ -35,53 +36,69 @@ def comp_eval(model_str, x_test, compression_ratios, snr, mode='multiple'):
         model_dic['Pred_Images'].append(pred_images)
         model_dic['PSNR'].append(psnr)
         model_dic['SSIM'].append(ssim)
+        print('Comp_Ratio = ', comp_ratio)
+        print('PSNR = ', psnr)
+        print('SSIM = ', ssim)
+        print('\n')
+
+        path = './compratio_txt/' + model_str + '_CompRatio{0}_SNR{1}.txt'.format(comp_ratio, snr)
+        with open(path, 'w') as f:
+            print(compression_ratios, '\n', model_dic['PSNR'], file=f)
+        f.closed
     return model_dic
 
+'''
+def test_eval(model_str, x_test, comp_ratio, snr, snr_test):
+    model_dic = {'Test_snr': [], 'PSNR': []}
+    for snr_t in snr_test:
+        tf.keras.backend.clear_session()
+        path = './checkpoints/' + model_str + '_CompRatio{0}_SNR{1}.h5'.format(comp_ratio, snr)
+        autoencoder = load_model(path, custom_objects={'NormalizationNoise': NormalizationNoise})
+        K.set_value(autoencoder.get_layer('normalization_noise_1').snr_db, snr_t)
 
+        pred_images = autoencoder.predict(x_test) * 255
+        pred_images = pred_images.astype('uint8')
+        psnr = peak_signal_noise_ratio(testX, pred_images)
+        model_dic['Test_snr'].append(snr_t)
+        model_dic['PSNR'].append(psnr)
+        print('Test SNR = ', snr_t)
+        print('PSNR = ', psnr)
+        print('\n')
+
+        path = './test_txt/' + model_str + '_CompRatio{0}_SNR{1}.txt'.format(comp_ratio, snr)
+        with open(path, 'w') as f:
+            print(snr_test, '\n', model_dic['PSNR'], file=f)
+        f.closed
+
+    return model_dic
+
+'''
 # plot
-def comp_plot(model_str, x_test, compression_ratios, snr_lst, title, x_lablel, y_label):
+def comp_plot(model_str, x_test, compression_ratios, snr_train):
     markers = ["*", "s", "o", "X", "d", "v", "<", ">", "^", "P", "H", "|"]
     colors = list(mcolors.TABLEAU_COLORS)
     history = []
 
     for model in model_str:
         i = 0
-        for snr in snr_lst:
+        for snr in snr_train:
             print('\n----> Now Getting Data and Preparing Plot for SNR {0} dB <----'.format(snr))
-            model_dic = comp_eval(model, x_test, compression_ratios, snr, mode='multiple')
+            model_dic = comp_eval(model, x_test, compression_ratios, snr) #mode='multiple'
             history.append(model_dic)
             label = model + ' (SNR={0}dB)'.format(snr)
             plt.plot(compression_ratios, model_dic['PSNR'], ls='--', c=colors[i], marker=markers[i], label=label)
             i += 1
 
-            plt.title(title)
-            plt.xlabel(x_lablel)
-            plt.ylabel(y_label)
+            plt.title('AWGN Channel')
+            plt.xlabel('k/n')
+            plt.ylabel('PSNR (dB)')
             plt.grid(True)
 
-        plt.ylim(11, 21)
+        #plt.ylim(11, 21)
         plt.legend(loc='lower right')
         plt.show()
-        return history
-
-####################################################################################################
-# test
-def snr_eval(model_str, x_test, comp_ratio, snr_test, snr_train):
-    model_dic = {'Train_snr': [snr_train], 'Test_snr': [], 'PSNR': []}
-    for snr in snr_test:
-        tf.keras.backend.clear_session()
-        path = './checkpoints/' + model_str + '_CompRatio{0}_SNR{1}/Autoencoder.h5'.format(comp_ratio, snr_train)
-        autoencoder = load_model(path, custom_objects={'NormalizationNoise': NormalizationNoise})
-        K.set_value(autoencoder.get_layer('normalization_noise_1').snr_db, snr)
-        pred_images = autoencoder.predict(x_test) * 255
-        pred_images = pred_images.astype('uint8')
-        psnr = peak_signal_noise_ratio(testX, pred_images)
-        model_dic['Test_snr'].append(snr)
-        model_dic['PSNR'].append(psnr)
-    return model_dic
-
-# plot
-def snr_plot(model_str, x_test, compression_ratio, snr_train, title, x_lablel, y_label):
+'''
+def test_plot(model_str, x_test, comp_ratio, snr_train, snr_test):
     markers = ["*", "s", "o", "X", "d", "v", "<", ">", "^", "P", "H", "|"]
     colors = list(mcolors.TABLEAU_COLORS)
     history = []
@@ -89,74 +106,36 @@ def snr_plot(model_str, x_test, compression_ratio, snr_train, title, x_lablel, y
         i = 0
         for snr in snr_train:
             print('\n----> Now Getting Data and Preparing Plot for SNR {0} dB <----'.format(snr))
-            model_dic = snr_eval(model, x_test, compression_ratio, snr_test, snr)
+            model_dic = test_eval(model, x_test, comp_ratio, snr, snr_test)
             history.append(model_dic)
             label = model + ' (SNR_train={0}dB)'.format(snr)
             plt.plot(snr_test, model_dic['PSNR'], ls='--', c=colors[i], marker=markers[i], label=label)
             i += 1
 
-            plt.title(title)
-            plt.xlabel(x_lablel)
-            plt.ylabel(y_label)
+            plt.title('AWGN Channel')
+            plt.xlabel('SNR_test (dB)')
+            plt.ylabel('PSNR (dB)')
             plt.grid(True)
 
         plt.ylim(11, 21)
         plt.legend(loc='lower right')
         plt.show()
-        return history
+
 
 
 ### 실행
 #plot1
 model_str = ['model1']
+'''
 compression_ratios = [0.06] #0.26, 0.49
 snr_train = [10] #0, 10, 20
 
-comp_plot(model_str, x_test, compression_ratios, snr_train, title='AWGN Channel', x_lablel='k/n', y_label='PSNR (dB)')
+comp_plot(model_str, x_test, compression_ratios, snr_train)
 
+'''
 #plot2
-snr_test = [2, 4, 7, 10, 13, 16, 18, 22, 25, 27]
 comp_ratio = 0.06
-snr_plot(model_str, x_test, comp_ratio, snr_train, title='AWGN Channel', x_lablel='SNR_test (dB)', y_label='PSNR (dB)')
+snr_train = [10]
+snr_test = [2, 4, 7, 10, 13, 16, 18, 22, 25, 27]
 
-
-
-'''
-def plot(channel, model_list):
-    color_list = list(mcolors.TABLEAU_COLORS)
-    ber_list = []
-    label_list = []
-
-    if channel == 'awgn':
-        path = './awgn/'
-    elif channel == 'bursty':
-        path = './bursty/'
-    else:
-        path = './rayleigh/'
-
-    for name in model_list:
-        with open(path + name + '.txt', 'r') as f:
-            text = f.read()
-            Vec_Eb_N0 = text.split('\n')[0]
-            Vec_Eb_N0 = json.loads(Vec_Eb_N0)
-            ber = text.split('\n')[1]
-            ber = json.loads(ber)
-        ber_list.append(ber)
-        label_list.append(name)
-
-
-    for i in range(len(model_list)):
-        plt.semilogy(Vec_Eb_N0, ber_list[i], label=label_list[i], color=color_list[i], marker='o')
-
-    plt.legend(loc=0)
-    plt.xlabel('Eb/N0(dB)')
-    plt.ylabel('BER')
-    plt.title(channel + ' Channel')
-    plt.grid('true')
-    plt.show()
-
-# RUN
-model_list = ['basic', 'model5']
-channel = 'bursty'
-plot(channel, model_list)
-'''
+test_plot(model_str, x_test, comp_ratio, snr_train, snr_test)
